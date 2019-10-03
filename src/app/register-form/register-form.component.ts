8fafda8091033844c4480e726936028e51f22bbd
rahul-coditas <rahul.raut@coditas.com>
2019-10-03 17:22:17 +0530

    Implemented form validation for entire registration form

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -10,6 +10,7 @@ import { Validators } from '@angular/forms';
 export class RegisterformComponent {
 
     title = 'Registeration Form';    
+
     profileForm = new FormGroup ({
         name: new FormGroup({
             firstName: new FormControl('', Validators.required),
@@ -17,50 +18,53 @@ export class RegisterformComponent {
             lastName: new FormControl('', Validators.required)
         }),
         contact: new FormGroup({
-            email: new FormControl('', Validators.required),
-            phone: new FormControl('', Validators.required),
-            wPhone: new FormControl('')
+            email: new FormControl('', {
+                validators: [
+                    Validators.required,
+                    Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+                ]
+             }),
+            phone: new FormControl('', {
+                validators: [
+                    Validators.required,
+                    Validators.pattern('^[0-9]{10,14}$')
+                ]
+            }),
+            wPhone: new FormControl('', {
+                validators: [
+                    Validators.required,
+                    Validators.pattern('^[0-9]{10,14}$')
+                ]
+            })
         }),
         location: new FormGroup({
             address: new FormControl('', Validators.required),
             country: new FormControl('', Validators.required),
             state: new FormControl('', Validators.required),
             city: new FormControl('', Validators.required),
-            zip: new FormControl('', Validators.required)
+            zip: new FormControl('', {
+                validators: [
+                    Validators.required,
+                    Validators.pattern('^[0-9]{6}$')
+                ]
+            })
         }),
         key: new FormGroup({
             password: new FormControl('', Validators.required),
             cPassword: new FormControl('', Validators.required)
-        })
+        }, { updateOn: 'change' })
     });
 
-    onSubmit() {
-        // console.log(this.profileForm);
-    }
+    nameFields = this.profileForm.controls.name;
+    contactFields = this.profileForm.controls.contact;
+    addressFields = this.profileForm.controls.location;
+    passwordFields = this.profileForm.controls.key;
     
+    onSubmit() {
+        console.log(this.profileForm.value);
+    }
+
     resetForm() {
-        this.profileForm.patchValue ({
-            name: {
-                firstName: "",
-                middleName: "",
-                lastName: ""
-            },
-            contact: {
-                email: "",
-                phone: "",
-                wPhone: ""
-            },
-            location: {
-                address: "",
-                country: "",
-                state: "",
-                city: "",
-                zip: ""
-            },
-            key: {
-                password: "",
-                cPassword: ""
-            }
-        });
+        this.profileForm.reset(''); 
     }
 }
